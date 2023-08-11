befaa68da02dc883a357f590b930517751b9bb47
pmad01 <patrickmadhi@gmail.com>
2023-08-11 20:44:59 +0200

    Revert "Update"
    This reverts commit 6d877c45d777eee43e5980f6e3f121ad921f2c34.
    
    "Update"

--- Zoomy.js
+++ Zoomy.js
@@ -77,16 +77,12 @@ class Zoomy {
 	 * @param {Event} e
 	 */
 	transformByMouseEvent(e) {
+
 		const currentMouseX = e.x;
 		const currentMouseY = e.y;
+
 		var moveXBy = 0;
 		var moveYBy = 0;
-		var img = this.el.getBoundingClientRect();
-		var box = this.boxEl.getBoundingClientRect();
-		var widthFits = img.width <= box.width;
-		var heightFits = img.height <= box.height;
-		var heightInsideCanvas = (img.top >= box.top && img.bottom <= box.bottom);
-		var widthInsideCanvas = (img.left >= box.left && img.right <= box.right);
 
 		this.isDragging = false;
 		if (e.type === 'mousedown') {
@@ -99,17 +95,15 @@ class Zoomy {
 			this.isDragging = true;
 		}
 		if (this.isDragging) {
-			if (!widthFits && !widthInsideCanvas) {
-					moveXBy += currentMouseX - this.lastMouseX;
-			}
-			if (!heightFits && !heightInsideCanvas) {
-				    moveYBy += currentMouseY - this.lastMouseY;
-			}
-
+			moveXBy += currentMouseX - this.lastMouseX;
+			moveYBy += currentMouseY - this.lastMouseY;
 		}
-
 		this.lastMouseX = currentMouseX;
 		this.lastMouseY = currentMouseY;
+
+
+		var img = this.el.getBoundingClientRect();
+		var box = this.boxEl.getBoundingClientRect();
 
 		var enlargeOrShrinkBy = 0;
 		var zoomFactor = .1;
@@ -174,7 +168,7 @@ class Zoomy {
 						});
 				}
 			} else {
-				if (widthFits && heightFits && widthInsideCanvas && heightInsideCanvas) {
+				if (this.imageFits()) {
 					newImageCenterXDiff = 0;
 					newImageCenterYDiff = 0;
 				}
@@ -226,6 +220,17 @@ class Zoomy {
 		m.scaleX += enlargeOrShrinkBy;
 		m.scaleY += enlargeOrShrinkBy;
 		this.el.style.transform = 'matrix('+Object.values(m).join(', ')+')';
+	}
+
+	/**
+	 *Checks if the image fits inside the box.
+	 * @return {boolean}
+	 */
+	imageFits() {
+		var img = this.el.getBoundingClientRect();
+		var box = this.boxEl.getBoundingClientRect();
+
+		return img.width <= box.width && img.height <= box.height;
 	}
 
 	/**
